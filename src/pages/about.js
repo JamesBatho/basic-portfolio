9c9bd10bf0eef8b6592f8c93ddb51fe3a1a973fa
James Batho <j.batho325@gmail.com>
2021-07-13 14:23:05 -0400

    basic portfolio site start

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h4>James Batho is someone you want to hire!</h4>
+      </div>
+    </Layout>
+  )
+}
