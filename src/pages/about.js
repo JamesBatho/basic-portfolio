f0f5dbca1f04de886b775d0e86e77f34a0c8e057
James Batho <j.batho325@gmail.com>
2021-07-19 16:02:49 -0400

    cropped resume

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,20 +15,20 @@ export default function About({ data }) {
   return (
     <Layout>
       <h4 className={header}>
-        UX Designer / React Developer / Softwarer Engineer
+        UX Designer / React Developer / Software Engineer
       </h4>
       <div className={about}>
         <p className={aboutMe}>
-          I am a Web Developer with a strong engineering background has recently
-          graduated from Springboard in June. Currently I am working as a
-          freelancer on Upwork, developing a project with Code for Baltimore and
-          looking for a full time position.
+          I am a Web Developer with a strong engineering background who has
+          recently graduated from Springboard in June. Currently I am working as
+          a freelancer on Upwork, developing a project with Code for Baltimore
+          and looking for a full time position.
           <br />
           <br />
-          Over the course of the bootcamp and several years of prior coding
-          experience through coursework and projects I have become proficient
-          in: HTML, CSS, JavaScript, Python, React, Node, Express, Redux,
-          Jasmine, Jest and other front and backend technologies.
+          Over the course of the bootcamp and several years of coding experience
+          through coursework and projects I have become proficient in:
+          Javascript, Python, CSS, HTML, React, Node, Express, Jasmine, Jest.
+          and other front and backend technologies.
           <br />
           <br />
           I'm looking for a full time position where I will be able to continue
@@ -39,7 +39,7 @@ export default function About({ data }) {
           <br />
           When I'm not focused on honing my skills as a software engineer you
           can often find me hiking on some part of the Appalachian Trail or in
-          the kitchen making dinner for my family.
+          the kitchen making dinner for my girlfriend.
           <br />
           <br />
           If you're looking for someone with these skills and a strong
