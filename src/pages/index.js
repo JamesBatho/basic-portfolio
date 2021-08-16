ae9a3e4c3c79c27fbc31b3c66393989d244380f3
James Batho <j.batho325@gmail.com>
2021-08-16 01:05:51 -0400

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import React from "react"
 import Layout from "../components/Layout"
 import { header, btn, banner } from "../styles/home.module.css"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Helmet } from "react-helmet"
 
 export default function Home({ data }) {
   const image = getImage(data.file)
 
   return (
     <Layout>
+      <Helmet>
+        <html lang="en" />
+        <title> James Batho Web Developer Portfolio</title>
+        <meta
+          name="google-site-verification"
+          content="eb98KeFHnXsdxwADobJapT8F7xgpI7jLThd_qHUzeeo"
+        />
+      </Helmet>
       <section className={header}>
         <div>
           <h2>Design </h2>
